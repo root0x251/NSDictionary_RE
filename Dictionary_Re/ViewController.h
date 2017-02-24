a2c43e302750560054cb769dc14a02a9320540b5
Slava <rtootik@gmail.com>
2017-02-24 09:54:24 +0300

    Initial Commit

---
+++ Dictionary_Re/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dictionary_Re
+//
+//  Created by Slava on 24.02.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Dictionary_Re/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Dictionary_Re
+//
+//  Created by Slava on 24.02.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
